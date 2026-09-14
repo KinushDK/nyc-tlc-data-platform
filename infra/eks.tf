92461732f23bb2404c9d5bc8868fdfd319a085c8
Dennis Kinuthia Kibunja <kinuthiadennis04@gmail.com>
2026-09-14 10:40:57 +0300

    Increase node group to 3x t3.small for Airflow capacity

--- infra/eks.tf
+++ infra/eks.tf
@@ -41,8 +41,8 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      max_size       = 4
+      desired_size   = 3
       instance_types = ["t3.small"]
     }
   }
